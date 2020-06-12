44e63738bc079e99fe7f8cc29b2d0d6dad79850a
marnob <35141620+marnob@users.noreply.github.com>
2020-06-12 17:33:47 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,79 @@
+function FormatDisk {
+    $disks = Get-Disk | Where-Object partitionstyle -eq 'raw' | Sort-Object number
+    $letters = 70..89 | ForEach-Object { [char]$_ }
+    $count = 0
+    $labels = "data1","data2"
+    foreach ($disk in $disks) {
+        $driveLetter = $letters[$count].ToString()
+        $disk | 
+        Initialize-Disk -PartitionStyle MBR -PassThru |
+        New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+        Format-Volume -FileSystem NTFS -NewFileSystemLabel $labels[$count] -Confirm:$false -Force
+        $count++
+    }
+}
+Function InstallBGInfo {
+    $url = 'https://live.sysinternals.com/Bginfo.exe'
+    $BGIFileURL = 'https://github.com/marnob/PowerShell/raw/master/Default.bgi'
+    $dstPath = "$ENV:ProgramFiles\BgInfo"
+    $TaskName = 'BgInfo'
+    #$dstPath = "$env:USERPROFILE\Desktop"
+    $EXE = "$dstPath\Bginfo.exe"
+    $BGI = "$dstPath\Default.bgi"
+    $ExeArguments = """$BGI"" /TIMER:0 /NOLICPROMPT"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    if (!(Test-Path -Path $dstPath)){
+        New-Item -Path $dstPath -ItemType Directory | Out-Null
+    }
+    # Download BgInfo 
+    try{
+        Write-Host "Downloading Bgingo.exe from https://live.sysinternals.com..." -NoNewline
+        Invoke-WebRequest -Uri $url -OutFile $EXE -ErrorAction Stop
+        Write-Host "Done!" -ForegroundColor Green
+    }
+    catch [System.IO.DirectoryNotFoundException]{
+        Write-Host "`nERROR: No permissions to write to $dstPath." -ForegroundColor Red
+        Exit 1
+    }
+    catch{
+        Write-Host "`nERROR: Unkonow error occured." -ForegroundColor Red
+        Exit 1
+    }
+    # Download Default.bgi
+    try{
+        Write-Host "Downloading Default.bgi from GitHub..." -NoNewline
+        Invoke-WebRequest -Uri $BGIFileURL -OutFile $BGI -ErrorAction Stop
+        Write-Host "Done!" -ForegroundColor Green
+    }
+    catch [System.IO.DirectoryNotFoundException]{
+        Write-Host "`nERROR: No permissions to write to $dstPath." -ForegroundColor Red
+        Exit 1
+    }
+    catch{
+        Write-Host "`nERROR: Unkonow error occured." -ForegroundColor Red
+        Exit 1
+    }
+    # Create Scheduled Task
+    Try{
+        Write-Host "Creating scheduled task..." -NoNewline
+        $stateChangeTrigger = Get-CimClass -Namespace ROOT\Microsoft\Windows\TaskScheduler -ClassName MSFT_TaskSessionStateChangeTrigger -ErrorAction stop
+        $triggers = @()
+        $triggers += New-ScheduledTaskTrigger -AtLogOn -ErrorAction Stop
+        $triggers += New-CimInstance -CimClass $stateChangeTrigger -Property @{StateChange = 3} -ClientOnly -ErrorAction stop
+        $triggers += New-CimInstance -CimClass $stateChangeTrigger -Property @{StateChange = 1} -ClientOnly -ErrorAction stop
+        $Action= New-ScheduledTaskAction -Execute """$EXE""" -Argument $ExeArguments -ErrorAction Stop
+        $Principal = New-ScheduledTaskPrincipal -GroupId 'Users'
+        $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries  -DontStopIfGoingOnBatteries
+        Register-ScheduledTask -TaskName $TaskName -Trigger $triggers -Action $Action -Settings $Settings -force -Principal $Principal -ErrorAction stop | Out-Null -ErrorAction stop
+        Write-Host "Done!" -ForegroundColor Green
+    }
+    Catch{
+        Write-Host "`nERROR: Unkonow error occured creating the Scheduled Task." -ForegroundColor Red
+        Exit 1
+    }
+    Start-ScheduledTask -TaskName $TaskName
+}
 new-item -Path c:\TestMarcel -ItemType Directory
+FormatDisk
+InstallBGInfo
